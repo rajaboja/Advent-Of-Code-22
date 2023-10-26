4f7f60c77dc245636524f49aedd0b0e35552cc93
rajaboja <51645766+rajaboja@users.noreply.github.com>
2023-10-26 22:32:07 +0000

    add utils

--- 2.py
+++ 2.py
@@ -1,1 +1,9 @@
+# https://adventofcode.com/2022/day/2
+
+from utils import get_data
+
+
+data_url = "https://adventofcode.com/2022/day/2/input"
+
+get_data(data_url)
 
---
+++ utils.py
@@ -0,0 +1,12 @@
+import os
+import requests
+from urllib.parse import urlsplit
+
+
+def get_data(url):
+    fp = "data/"+ urlsplit(url).path.split('/')[-2]+'.txt'
+    if not os.path.isfile(fp):
+        data = requests.get(url,cookies={'session':os.environ.get('SESSION_ID')}).content
+        os.makedirs(os.path.dirname(fp), exist_ok=True)
+        with open(fp,'wb') as f:
+            f.write(data)
