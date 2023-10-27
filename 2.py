70e5a723d389813ae4ef3528e3da3130f7860f7f
rajaboja <51645766+rajaboja@users.noreply.github.com>
2023-10-27 22:58:32 +0000

    solve second part

--- 2.py
+++ 2.py
@@ -4,23 +4,34 @@ from utils import get_data
 
 
 data_url = "https://adventofcode.com/2022/day/2/input"
-scores_dict = dict(X=1,Y=2,Z=3)
-aliases = dict(X='A',Y='B',Z='C')
-rules = ['a>c','c>b','b>a']
 
+scores_dict = dict(A=1,B=2,C=3)
+aliases = dict(X='A',Y='B',Z='C')
+rules = ['A>C','C>B','B>A']
+outcome_scores = dict(X=0,Y=3,Z=6)
 
 def get_score(them,me):
-    score = scores_dict[me]
     me = aliases[me]
+    score = scores_dict[me]
     result = 0
     if them==me:
         result=3
-    if f"{me}>{them}".lower() in rules:
+    if f"{me}>{them}" in rules:
         result=6
     return score+result
 
-
-
+def get_score_2(them,outcome):
+    score = outcome_scores[outcome]
+    if outcome=='Y':
+        me = them
+    elif outcome=='Z':
+        string = '>'+them
+        me = [i.strip(string) for i in rules if i.endswith(string) ][0]
+    else:
+        string = them+'>'
+        me = [i.strip(string) for i in rules if i.startswith(string)][0]
+    score += scores_dict[me]
+    return score
 
 if __name__=="__main__":
     fp = get_data(data_url)
@@ -30,4 +41,8 @@ if __name__=="__main__":
     data_arr = [i.strip().split(' ') for i in data.split('\n') if i]
     answer_1 = sum(get_score(*arr) for arr in data_arr)
     print(answer_1)
+
+    answer_2 = sum(get_score_2(*arr) for arr in data_arr)
+    print(answer_2)
+
 
--- utils.py
+++ utils.py
@@ -5,8 +5,11 @@ from urllib.parse import urlsplit
 
 def get_data(url):
     fp = "data/"+ urlsplit(url).path.split('/')[-2]+'.txt'
+    session_id = os.environ.get('SESSION_ID')
     if not os.path.isfile(fp):
-        data = requests.get(url,cookies={'session':os.environ.get('SESSION_ID')}).content
+        if not session_id:
+            raise KeyError('session_id not found') 
+        data = requests.get(url,cookies={'session':session_id}).content
         os.makedirs(os.path.dirname(fp), exist_ok=True)
         with open(fp,'wb') as f:
             f.write(data)
