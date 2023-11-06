53e36337d4a65cb51ca9b93fe67c76f715411745
rajaboja <51645766+rajaboja@users.noreply.github.com>
2023-11-06 09:36:14 +0000

    day 9 refactored

--- scripts/9.py
+++ scripts/9.py
@@ -1,42 +1,54 @@
 from utils import get_data
 
+
 url = "https://adventofcode.com/2022/day/9/input"
 sign = {'R':1,'U':1,'D':-1,'L':-1}
 
-def new_coordinates(tail,head,move):
-    direction,n = move.strip().split(' ')
-    value = sign[direction]
-    coord = direction not in 'RL'
-    tails = []
-    for _ in range(int(n)):
-        head[coord]+= value
-        if max(abs(a-b) for a,b in zip(tail,head))>1:
-            if all([head[0]!=tail[0],head[1]!=tail[1]]):
-                tail[coord]+=value
-                tail[not coord]=head[not coord]
-            else:
-                tail[coord]+=value
-            tails.append(tuple(tail))
-    return tail,head,tails
 
+
+def n_new_coordinates(start_coords,move):
+    direction,n = move.strip().split(' ')
+    coord = direction not in 'RL'
+    value = sign[direction]
+    tails = []
+
+    for _ in range(int(n)):
+        head=start_coords[0]
+        head[coord]+= value
+        start_coords[0] = head
+
+        for i in range(1,len(start_coords)):
+            tail = start_coords[i]
+            x,y = [(a-b) for a,b in zip(head,tail)]
+            if max(map(abs,[x,y]))>1:
+                tail[0]+=x//(abs(x) or 1)
+                tail[1]+=y//(abs(y) or 1)
+
+                start_coords[i]=tail
+            head = tail
+        tails.append(tuple(start_coords[-1]))
+    return start_coords,tails
 
     
+def unique_points(data,n):
+    all_tails = []
+    start_coords = [[0,0] for _ in range(n)]
+
+    for i in data:
+        start_coords, tails = n_new_coordinates(start_coords,i)
+        all_tails.extend(tails)
+            
+    return len(set(all_tails))
 
 
 if __name__=='__main__':
     fp = get_data(url)
     with open(fp) as f:
         data = f.readlines()
-    head=[0,0]
-    tail=[0,0]
-    all_tails = []
-    
-    for i in data:
-        tail, head, tails = new_coordinates(tail,head,i)
-        all_tails.extend(tails)
-            
-    answer_1 = len(set(all_tails))+1
-    print(answer_1)
+
+    print(unique_points(data,2))
+    print(unique_points(data,10))
+
             
     
 
